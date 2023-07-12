c64d83369eaecab39f495697f3418d305a4632f9
parkshinhae <parkshinhae@naver.com>
2023-07-12 17:52:50 +0900

    메인페이지작업

--- src/component/BookMarkList.js
+++ src/component/BookMarkList.js
@@ -1,17 +1,18 @@
 import ShoppingItem from "./ShoppingItem";
 
 function BookMarkList( {allData} ) {
-      if(allData.length === 0) {
-        return null;
-      } else {
-        return (
-          <div className="Shop__container">
-          {(allData.slice(5, 9)).map((item) => (
-          <ShoppingItem item={item} />
-          ))}
-          </div>
-        )
-      }
+  if(allData.length === 0){
+    return null;
+} else {
+    return(
+    <div className="shop__container">
+        <div className="shopping_name">상품 리스트</div>
+        {(allData.slice(0, 4)).map((item) => (
+        <ShoppingItem item={item} />
+        ))}
+    </div>
+    )
+}
 };
   
 export default BookMarkList;
--- src/component/ShoppingList.js
+++ src/component/ShoppingList.js
@@ -3,16 +3,16 @@ import ShoppingItem from "./ShoppingItem";
 function ShoppingList( {allData} ) {
     if(allData.length === 0){
         return null;
-      }else {
+    } else {
         return(
-            <div className="Shop__container">
-                <div className="shopping_name">상품 리스트</div>
-                {(allData.slice(0, 4)).map((item) => (
-                <ShoppingItem item={item} />
-                ))}
-            </div>
-  )
-}
+        <div className="shop__container">
+            <div className="shopping_name">상품 리스트</div>
+            {(allData.slice(0, 4)).map((item) => (
+            <ShoppingItem item={item} />
+            ))}
+        </div>
+        )
+    }
 };
-  
+
 export default ShoppingList;
