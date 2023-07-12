f34f96a20f4aa636b8a84d9a70ba6287b451fd97
parkshinhae <parkshinhae@naver.com>
2023-07-12 18:05:01 +0900

    오타 수정

--- src/component/ShoppingItem.js
+++ src/component/ShoppingItem.js
@@ -1,5 +1,5 @@
 function ShoppingItem( {item} ) {
-  console.log(item)
+  console.log(item);
   return(
     <div className="image__box" style={{width: "300px", height: "300px"}}>
       <img className="shopping__img" src={item.image_url || item.brand_image_url} />
